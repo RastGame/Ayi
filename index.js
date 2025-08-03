72fd575ce47702fa32b269083bf495fe5162f07e
RastGame <rastgame1@gmail.com>
2025-08-03 17:58:05 +0300

    now in host

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 import { Client } from 'yurba.js';
+import pkg from './package.json' with { type: "json" };
+const { version, author } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
 const client = new Client(process.env.TOKEN);
 
 client.on('ready', () => {
-  console.log('Бот запущено з middleware:');
+  console.log(`Бот запущено: ${client.user.Name}`);
 });
 
 client.registerCommand('hello', {}, (message) => {
   message.reply(`Hello! @${message.Author.Link}`)
+})
+
+client.registerCommand('info', {}, (message) => {
+  message.reply(`Інформація про мене: \n- Ім'я: ${client.user.Name}\n- Розробник: @${author}\n- Версія: ${version}`)
 })
 
 client.init();
