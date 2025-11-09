a97896c14b473d26db3673a8bf96cbe1667bdf5d
RastGame <rastgame1@gmail.com>
2025-11-09 21:12:21 +0200

    update

--- src/events/join.js
+++ src/events/join.js
@@ -9,9 +9,12 @@ export default {
     // Перевірка чи є користувач ботом
     if (message.Author?.Link?.endsWith('_bot')) return;
     
+    const member = await client.api.users.get(message.Author.ID);
+    const dialog = await client.api.dialogs.get(message.Dialog.ID);
+
     const welcomer = await Welcomer.findById(message.Dialog.ID);
     if (welcomer?.welcome?.enabled && welcomer.welcome.text) {
-      const response = replacePlaceholders(welcomer.welcome.text, message);
+      const response = replacePlaceholders(welcomer.welcome.text, member, dialog);
       await client.sendMessage(message.Dialog.ID, { text: response, replyTo: message.ID});
     }
     
--- src/events/leave.js
+++ src/events/leave.js
@@ -10,9 +10,12 @@ export default {
     // Видаляємо користувача з бази даних
     await User.deleteByDialogAndUser(message.Dialog.ID, message.Author.ID);
     
+    const member = await client.api.users.get(message.Author.ID);
+    const dialog = await client.api.dialogs.get(message.Dialog.ID);
+
     const welcomer = await Welcomer.findById(message.Dialog.ID);
     if (welcomer?.goodbye?.enabled && welcomer.goodbye.text) {
-      const response = replacePlaceholders(welcomer.goodbye.text, message);
+      const response = replacePlaceholders(welcomer.goodbye.text, member, dialog);
       await client.sendMessage(message.Dialog.ID, { text: response, replyTo: message.ID });
     }
   }
